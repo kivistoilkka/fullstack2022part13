d6ed99bb346ab7058fc19432a64fa926a573cdfb
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-01-11 17:56:30 +0200

    Exercise 13.6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,12 +17,26 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    await blog.destroy()
+const blogFinder = async (req, _res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
   }
   res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
