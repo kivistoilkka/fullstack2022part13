f64b715a4fa2780b9725c7fcd346b503fa63e52e
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-02-22 20:19:22 +0200

    Exercise 13.10

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,32 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 const router = require('express').Router()
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 router.get('/', async (_req, res) => {
   const blogs = await Blog.findAll()
   res.json(blogs)
 })
 
-router.post('/', async (req, res) => {
-  console.log(req.body)
-  const blog = await Blog.create(req.body)
+router.post('/', tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id)
+  const blog = await Blog.create({ ...req.body, userId: user.id })
   res.json(blog)
 })
 
