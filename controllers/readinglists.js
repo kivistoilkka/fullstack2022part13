447bae03d577610908407696343711fc2f5fbb15
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-03-13 21:05:06 +0200

    Exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 
 const { Readinglistitem } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
   const item = await Readinglistitem.create({
@@ -8,6 +9,23 @@ router.post('/', async (req, res) => {
     userId: req.body.user_id,
   })
   res.json(item)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+  const item = await Readinglistitem.findByPk(req.params.id)
+  if (item) {
+    if (!(req.decodedToken.id === item.userId)) {
+      res.status(401).json({
+        error: 'only the owner of the reading list can update read status',
+      })
+    } else {
+      item.read = req.body.read
+      await item.save()
+      res.json(item)
+    }
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
+
 const unknownEndpoint = (_req, res) => {
   res.status(404).send({ error: 'unknown endpoint' })
 }
@@ -17,4 +20,19 @@ const errorHandler = (error, _req, res, next) => {
   } else next(error)
 }
 
-module.exports = { unknownEndpoint, errorHandler }
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+module.exports = { unknownEndpoint, errorHandler, tokenExtractor }
