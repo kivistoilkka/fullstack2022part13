92ef927159da5c3a73b37b93b719fd9d0722e8d3
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-03-13 19:35:59 +0200

    Exercise 13.19

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Blog = require('./blog')
 const User = require('./user')
+const Readinglistitem = require('./readinglistitem')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: Readinglistitem, as: 'savedBlogs' })
+Blog.belongsToMany(User, { through: Readinglistitem, as: 'usersSaved' })
+
 module.exports = {
   Blog,
   User,
+  Readinglistitem,
 }
