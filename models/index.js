239255eec0a23ceba724aeaea91e9793aeb02a59
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-03-14 12:24:52 +0200

    Exercise 13.24, new column in users and new table active_sessions with migration

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Blog = require('./blog')
 const User = require('./user')
 const Readinglistitem = require('./readinglistitem')
+const ActiveSession = require('./activeSession')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
@@ -8,8 +9,11 @@ Blog.belongsTo(User)
 User.belongsToMany(Blog, { through: Readinglistitem, as: 'readings' })
 Blog.belongsToMany(User, { through: Readinglistitem, as: 'usersSaved' })
 
+User.hasOne(ActiveSession)
+
 module.exports = {
   Blog,
   User,
   Readinglistitem,
+  ActiveSession,
 }
