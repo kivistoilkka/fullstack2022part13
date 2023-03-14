a70a3ee06807bbd35908af376070b470d5b541c1
Ilkka Kivistö <ilkka.kivisto@gmail.com>
2023-03-14 14:14:30 +0200

    Exercise 13.24, fixed migration

--- migrations/20230314_00_disabled_to_users_and_add_active_sessions.js
+++ migrations/20230314_00_disabled_to_users_and_add_active_sessions.js
@@ -21,9 +21,18 @@ module.exports = {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     })
   },
   down: async ({ context: queryInterface }) => {
     await queryInterface.removeColumn('users', 'disabled')
+    await queryInterface.dropTable('active_sessions')
   },
 }
